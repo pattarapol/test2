40c2b107b4ff84c50d1b73a953ec21b3cc760327
RomeroLuis <romuis.92@gmail.com>
2013-10-24 11:00:26 -0700

    Update date2.cpp
    i wanted t see if i can change the code... line 6

--- date2.cpp
+++ date2.cpp
@@ -3,7 +3,7 @@
 #include<fstream>
 #include"month.h"
 #include"date.h"
-
+// lets see if this allows me to directly change the code 
 using namespace std;
 const int DAYSINMONTH[2][13]={{0,31,59,90,120,151,181,212,243,273,304,334,365},
                               {0,31,60,91,121,152,182,213,244,274,305,335,366}};
